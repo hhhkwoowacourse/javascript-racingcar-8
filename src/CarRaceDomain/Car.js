dbd34ca70a705d313867a64a0169c707974ef0e0
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-10-26 01:52:06 +0900

    feat: Car 클래스 구현

---
+++ src/CarRaceDomain/Car.js
@@ -0,0 +1,26 @@
+import { Random } from "@woowacourse/mission-utils";
+
+export default class Car {
+  #name;
+  #position;
+
+  /**
+   * 생성자 함수
+   * @param {string} name 
+   * @param {number} postion
+   */
+  constructor(name) {
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  /**
+   * 랜덤한 수가 4보다 클 경우에 전진하는 함수
+   */
+  proceedCar() {
+    if (Random.pickNumberInRange(0, 9) >= 4) this.#position++;
+  }
+
+  getName = () => this.name;
+  getPosition = () => this.position;
+}
