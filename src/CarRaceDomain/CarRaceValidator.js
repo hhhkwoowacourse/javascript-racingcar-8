d7777c65e57f0911ec4d28beac8b9212e83ed8a0
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-10-26 23:18:13 +0900

    feat: Validator 구현

--- src/CarRaceDomain/CarRaceValidator.js
+++ src/CarRaceDomain/CarRaceValidator.js
@@ -1,14 +1,46 @@
 export default class CarRaceValidator {
   constructor() {}
-  // Validator
+  // Validator for Car List
   validateCarList(carListInput) {
-    try {
-      const carList = carListInput.split(",");
-    } catch {}
+    validateEndsWithComma(carListInput);
+    validateDuplicateName(carListInput);
   }
 
+  // Validator for Round Number
   validateRoundNumber(roundNumberInput) {
-    try {
-    } catch {}
+    validateNaN(roundNumberInput);
+    validatePositiveNumber(roundNumberInput);
   }
+
+  // Validator for Car Name
+  validateCarName(carName) {
+    validateCarNameLength(carName);
+  }
+}
+
+function validateEndsWithComma(input) {
+  if (!input.endsWith(","))
+    throw new Error("[ERROR] 쉼표로 마무리될 수 없습니다.");
+}
+
+function validateDuplicateName(input) {
+  const carNames = input.split(",");
+  const uniqueCarNames = new Set(carNames);
+
+  if (carNames.length !== uniqueCarNames.size)
+    throw new Error("[ERROR] 중복된 자동차 이름이 있습니다.");
+}
+
+function validateNaN(input) {
+  if (Number(input) === NaN)
+    throw new Error("[ERROR] 올바른 숫자를 입력해주세요.");
+}
+
+function validatePositiveNumber(input) {
+  if (Number(input) <= 0) throw new Error("[ERROR] 양수를 입력해주세요.");
+}
+
+function validateCarNameLength(carName) {
+  if (carName.length > 5)
+    throw new Error("[ERROR] 자동차 이름을 5자 이하로 입력해주세요.");
 }
